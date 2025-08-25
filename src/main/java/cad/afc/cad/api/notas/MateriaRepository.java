b41bdbca5c9963e2b73724962a8886a8805e943e
Enso340 <enzocorcetti4@hotmail.com>
2025-08-24 23:54:23 -0300

    feat: Adicionada entidade Materia e logica de faltas por materia

--- src/main/java/cad/afc/cad/api/notas/MateriaRepository.java
+++
@@ -1,7 +0,0 @@
-package cad.afc.cad.api.notas;
-
-import cad.afc.cad.api.materia.Materia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MateriaRepository extends JpaRepository<Materia, Long> {
-}
